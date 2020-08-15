b13bf63d72ab36078498bf1dcecc16bf112e8746
Regan Layman <regan.layman@gmail.com>
2020-08-15 16:53:06 -0400

    added in video in frame by creating a canvas element

--- gaze.js
+++ gaze.js
@@ -14,3 +14,53 @@ window.onload = function() {
   //shows a square every 100 milliseconds where prediction is//
 
 }
+
+/* TO SETUP A VDEO IN A FRAME */
+
+let width = 320;
+let height = 240;
+let topDist = 'Opx';
+let leftDist = '0px';
+
+let setup = function(){
+  
+  let video = document.getElementById('webgazerVideoFeed');
+  video.style.display = 'block';
+  video.style.position = 'absolute';
+  video.style.top = topDist;
+  video.style.left = leftDist;
+  video.width = width;
+  video.height = height;
+  video.style.margin = ‘0px’;
+  
+  webgazer.params.imgWidth = width;
+  webgazer.params.imgHeigh = height;
+  
+  let overlay = document.createElement('canvas');
+  overloay.id = 'overlay';
+  overlay.style.position = ‘absolute’;
+  overlay.width = width;
+  overlay.height = height;
+  overlay.style.top = topDist;
+  overlay.style.left = leftDist;
+  overlay.style.margin = ‘0px’;
+  
+  let myCanvas = get('canvas');
+  myCanvas.width=screen.width;
+  myCanvas.height=screen.height;
+  
+  document.body.appendChild(overlay);
+  
+  let cl = webgazer.getTracker().clm;
+  
+  function drawLoop(){
+    
+    requestAnimationFrame(drawLoop);
+    overlay.getContext('2d').clearRect(0,0,width,height);
+    
+      if (cl.getCurrentPosition()) {
+        cl.draw(overlay);
+      }
+  }
+  drawLoop();
+};
