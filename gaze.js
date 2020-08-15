86ccb59016bd338af559098cd87c18b53ca19073
Regan Layman <regan.layman@gmail.com>
2020-08-15 16:20:01 -0400

    added in file for webgazer.js and a button for stoptracking()

--- gaze.js
+++ gaze.js
@@ -5,3 +5,16 @@ window.applyKalmanFilter = true;
 //Don't save data on page reload, each instance is new, no database
 window.saveDataAcrossSessions = false;
 
+window.onload = async function() {
+  
+  webgazer.params.showVideoPreview = true;
+  const webgazerInstance = await webgazer.setRegression('ridge')
+  .setTracker('TFFacemesh')
+  .begin();
+  
+  webgazerInstance.showPredictionPoints(false);  
+  /* shows a square every 100 milliseconds where current prediction is */
+  
+  // Add the SVG component on the top of everything.
+  setup
+}
