fdb001b8a3d6d539570f8dd832d991d10500c976
Regan Layman <regan.layman@gmail.com>
2020-08-15 16:59:21 -0400

    moved JS code into the original onload brackets and added a check to see if the webgazer is ready so it can call the setup method

--- gaze.js
+++ gaze.js
@@ -13,54 +13,67 @@ window.onload = function() {
   .showPredictionPoints(true); 
   //shows a square every 100 milliseconds where prediction is//
 
-}
+  /* TO SETUP A VDEO IN A FRAME */
 
-/* TO SETUP A VDEO IN A FRAME */
+  let width = 320;
+  let height = 240;
+  let topDist = 'Opx';
+  let leftDist = '0px';
 
-let width = 320;
-let height = 240;
-let topDist = 'Opx';
-let leftDist = '0px';
+  let setup = function(){
 
-let setup = function(){
-  
-  let video = document.getElementById('webgazerVideoFeed');
-  video.style.display = 'block';
-  video.style.position = 'absolute';
-  video.style.top = topDist;
-  video.style.left = leftDist;
-  video.width = width;
-  video.height = height;
-  video.style.margin = ‘0px’;
-  
-  webgazer.params.imgWidth = width;
-  webgazer.params.imgHeigh = height;
-  
-  let overlay = document.createElement('canvas');
-  overloay.id = 'overlay';
-  overlay.style.position = ‘absolute’;
-  overlay.width = width;
-  overlay.height = height;
-  overlay.style.top = topDist;
-  overlay.style.left = leftDist;
-  overlay.style.margin = ‘0px’;
-  
-  let myCanvas = get('canvas');
-  myCanvas.width=screen.width;
-  myCanvas.height=screen.height;
-  
-  document.body.appendChild(overlay);
-  
-  let cl = webgazer.getTracker().clm;
-  
-  function drawLoop(){
-    
-    requestAnimationFrame(drawLoop);
-    overlay.getContext('2d').clearRect(0,0,width,height);
-    
-      if (cl.getCurrentPosition()) {
-        cl.draw(overlay);
-      }
-  }
-  drawLoop();
+    let video = document.getElementById('webgazerVideoFeed');
+    video.style.display = 'block';
+    video.style.position = 'absolute';
+    video.style.top = topDist;
+    video.style.left = leftDist;
+    video.width = width;
+    video.height = height;
+    video.style.margin = ‘0px’;
+
+    webgazer.params.imgWidth = width;
+    webgazer.params.imgHeigh = height;
+
+    let overlay = document.createElement('canvas');
+    overloay.id = 'overlay';
+    overlay.style.position = ‘absolute’;
+    overlay.width = width;
+    overlay.height = height;
+    overlay.style.top = topDist;
+    overlay.style.left = leftDist;
+    overlay.style.margin = ‘0px’;
+
+    let myCanvas = get('canvas');
+    myCanvas.width=screen.width;
+    myCanvas.height=screen.height;
+
+    document.body.appendChild(overlay);
+
+    let cl = webgazer.getTracker().clm;
+
+    function drawLoop(){
+
+      requestAnimationFrame(drawLoop);
+      overlay.getContext('2d').clearRect(0,0,width,height);
+
+        if (cl.getCurrentPosition()) {
+          cl.draw(overlay);
+        }
+    }
+    drawLoop();
+  };
+
+  /* Check if webgazer is ready, call setup method */
+
+  function checkIfReady() {
+
+    if (webgazer.isReady()) {
+      setup();
+    } else {
+      setTimeout(checkIfReady, 100);
+    }
+
+  }  
+  setTimeout(checkIfReady, 100);
 };
+
