4df18eec3a0c1651e81685e1c5a42186b133713b
Regan Layman <regan.layman@gmail.com>
2020-08-15 20:02:34 -0400

    added in tracker module and regression, fixed <script> tag in HTML. I left an async i found in an example in there incorrectly

--- gaze.js
+++ gaze.js
@@ -1,99 +1,21 @@
-window.onload = function() {
-  
-  webgazer.setRegression('weightedRidge') //ridge, weightedRidge, threadedRidge, linear
-  
-  webgazer.setTracker("TFFacemesh"); //set tracker module clmtracker
-  
-  webgazer.setGazeListener(function (data, clock) {
-    // data is object containing x and y key, prediction coordinates
-    // clock is elapsed time in milliseconds since webgazer.begin() was called
-    
-  }).begin()
-  
-  .showPredictionPoints(true); 
-  //shows a square every 100 milliseconds where prediction is//
-
-  /* TO SETUP A VDEO IN A FRAME */
-
-  let width = 320;
-  let height = 240;
-  let topDist = 'Opx';
-  let leftDist = '0px';
-
-  let setup = function(){
-
-    let video = document.getElementById('webgazerVideoFeed');
-    video.style.display = 'block';
-    video.style.position = 'absolute';
-    video.style.top = topDist;
-    video.style.left = leftDist;
-    video.width = width;
-    video.height = height;
-    video.style.margin = ‘0px’;
-
-    webgazer.params.imgWidth = width;
-    webgazer.params.imgHeigh = height;
-
-    let overlay = document.createElement('canvas');
-    overloay.id = 'overlay';
-    overlay.style.position = ‘absolute’;
-    overlay.width = width;
-    overlay.height = height;
-    overlay.style.top = topDist;
-    overlay.style.left = leftDist;
-    overlay.style.margin = ‘0px’;
-
-    let myCanvas = get('canvas');
-    myCanvas.width=screen.width;
-    myCanvas.height=screen.height;
-
-    document.body.appendChild(overlay);
-
-    let cl = webgazer.getTracker().clm;
-
-    function drawLoop(){
-
-      requestAnimationFrame(drawLoop);
-      overlay.getContext('2d').clearRect(0,0,width,height);
-
-        if (cl.getCurrentPosition()) {
-          cl.draw(overlay);
-        }
+webgazer.setGazeListener(function(data, elapsedTime) {
+    if (data == null) {
+        return;
     }
-    drawLoop();
-  };
+    var xprediction = data.x; //these x coordinates are relative to the viewport
+    var yprediction = data.y; //these y coordinates are relative to the viewport
+    console.log(elapsedTime); //elapsed time is based on time since begin was called
+}).begin();
 
-  /* Check if webgazer is ready, call setup method */
+webgazer.setTracker("TFFacemesh"); //set a tracker module
 
-  function checkIfReady() {
+webgazer.addTrackerModule("newTracker", NewTrackerConstructor); //add a new tracker module
 
-    if (webgazer.isReady()) {
-      setup();
-    } else {
-      setTimeout(checkIfReady, 100);
-    }
+webgazer.setRegression("threadedRidge"); //set a regression module
+//other options: ridge, weightedRidge, threadedRidge
 
-  }  
-  setTimeout(checkIfReady, 100);
-};
+webgazer.addRegressionModule("newReg", NewRegConstructor); //add a new regression module
 
-window.onbeforeunload = function(){
-  
-  webgazer.end(); //saves the data even if you reload the page
-  window.localStorage.clear(); //deletes the data for a fresh session or reload
-  
-}
-
-/* HELPER FUNCTION */
-
-function get(id) {
-  
-  return document.getElementById(id);
-  
-}
-
-function stopTracking() {
-  
-  webgazer.end(); //Save webgaze data  
-  
-}
+webgazer.util.bound(prediction);
+prediction.x;
+prediction.y;
