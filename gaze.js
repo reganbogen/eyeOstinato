5574fb05c2adbd43069b813ec88f6f8c6c63439a
Regan Layman <regan.layman@gmail.com>
2020-08-15 22:36:50 -0400

    removed the added modules, removed the prediction point

--- gaze.js
+++ gaze.js
@@ -1,23 +1,37 @@
+var myX;
+var myY;
+
 webgazer.setGazeListener(function(data, elapsedTime) {
     if (data == null) {
         return;
     }
-    var xprediction = data.x; //these x coordinates are relative to the viewport
-    var yprediction = data.y; //these y coordinates are relative to the viewport
+    myX = data.x; //these x coordinates are relative to the viewport
+    myY = data.y; //these y coordinates are relative to the viewport
     console.log(elapsedTime); //elapsed time is based on time since begin was called
 }).begin();
 
-webgazer.setTracker("TFFacemesh"); //set a tracker module
+webgazer.showPredictionPoints(false);
 
-webgazer.addTrackerModule("newTracker", NewTrackerConstructor); //add a new tracker module
+webgazer.setTracker("TFFacemesh"); //set a tracker module
 
 webgazer.setRegression("weightedRidge"); //set a regression module
 //other options: ridge, weightedRidge, threadedRidge
 
-webgazer.addRegressionModule("newReg", NewRegConstructor); //add a new regression module
-
-webgazer.util.bound(prediction);
-prediction.x;
-prediction.y;
-
 window.saveDataAcrossSessions = false;
+
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+  background(234);
+}
+
+function draw(){
+  background(234);
+  ellipse(myX, myY, 40, 40);
+}
+
+window.onbeforeunload = function(){
+  
+  webgazer.end(); //saves the data even if you reload the page
+  window.localStorage.clear(); //deletes the data for a fresh session or reload
+  
+}
